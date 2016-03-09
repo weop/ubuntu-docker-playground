e03dc42e5bea5e5b7896473a7f8dbe0141fed609
weop <weop@users.noreply.github.com>
2016-03-09 18:32:41 +0500

    ddaemon

--- Vagrantfile
+++ Vagrantfile
@@ -11,8 +11,9 @@ Vagrant.configure("2") do |config|
   config.vm.hostname = "dock3r.localhost"
   config.vm.network :private_network, ip: "192.168.10.10"
 
-  #allow incoming connections on docker port
-  #config.vm.network :forwarded_port, host: 2375, guest: 2375, auto_correct: true
+  #allow incoming connections on docker host
+  #config.vm.network :forwarded_port, host: 8080, guest: 80, auto_correct: true
+  config.vm.network :forwarded_port, host: 2375, guest: 2375, auto_correct: true
 
   #starts provision
   config.vm.provision "run", type: "shell" do |s|
--- init.sh
+++ init.sh
@@ -3,9 +3,11 @@ sudo apt-get update && sudo apt-get upgrade -y
 
 echo "Adding Docker Repo"
 sudo apt-get install apt-transport-https ca-certificates -y
-sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D 
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 \
+--recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 sudo touch /etc/apt/sources.list.d/docker.list
-sudo echo 'deb https://apt.dockerproject.org/repo ubuntu-trusty main' > /etc/apt/sources.list.d/docker.list
+sudo echo 'deb https://apt.dockerproject.org/repo ubuntu-trusty main' \
+> /etc/apt/sources.list.d/docker.list
 
 echo "Pre-Install Tasks..."
 sudo apt-get update && sudo apt-cache policy docker-engine
@@ -18,3 +20,7 @@ echo "Running Post-Install Tasks"
 #Ubuntu 15.04
 #sudo systemctl enable docker
 sudo usermod -aG docker vagrant
+echo "Starting Docker Daemon..."
+sudo /usr/bin/docker daemon -H tcp://0.0.0.0:2375 \
+-H unix:///var/run/docker.sock \
+> /dev/null &
